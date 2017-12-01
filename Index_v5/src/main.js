501a3858cd78d55a8e5ab9f711e74cfe090b92c8
edwindmello <32240069+edwindmello@users.noreply.github.com>
2017-12-01 15:29:22 +0100

    these are the assets for the website

---
+++ Index_v5/src/main.js
@@ -0,0 +1,157 @@
+// var body = document.querySelector("body");
+
+// body.addEventListener("keypress", function(){
+// 	if(this.keyCode == 32){
+// 		;
+// 	}
+// });
+
+
+//var x = document.getElementById("myAudio");
+//var z = document.getElementById("myAudio2");
+
+//var y = keydown.keyCode;
+
+//function myFunction() {
+//    document.getElementById("myDIV").style.opacity = "1";
+//}
+
+
+document.addEventListener("keyup", keyupFunction);
+document.addEventListener("keydown", keydownFunction);
+
+// var doc = document.addEventListener("")
+
+
+
+// PLAYING AUDIO FILE ON PRESSING KEYS: A - S - D - F - G - H - J - K - L
+
+function keydownFunction(e) {
+	//var y = e.key;
+	if(e.which == 65){/* a */
+		document.getElementById("video1").style.opacity = "1";
+		myAudio.volume = 1.0;
+		
+	 } else if (e.which == 83) {/* s */
+	 	document.getElementById("video2").style.opacity = "1";
+	 	myAudio2.volume = 1.0;
+	 	
+	 } else if (e.which == 68) {/* d */
+	 	document.getElementById("video3").style.opacity = "1";
+	 	myAudio3.volume = 1.0;
+
+	 } else if (e.which == 70) {/* f */
+	 	document.getElementById("video4").style.opacity = "1";
+	 	myAudio4.volume = 1.0;
+	 	
+	 } else if (e.which == 71) {/* g */
+	 	document.getElementById("video5").style.opacity = "1";
+	 	myAudio5.volume = 1.0;
+
+	 } else if (e.which == 72) {/* h */
+	 	document.getElementById("video6").style.opacity = "1";
+	 	myAudio6.volume = 1.0;
+	 	
+	 } else if (e.which == 74) {/* i */
+	 	document.getElementById("video7").style.opacity = "1";
+	 	myAudio7.volume = 1.0;
+
+	 } else if (e.which == 75) {/* j */
+	 	document.getElementById("video8").style.opacity = "1";
+	 	myAudio8.volume = 1.0;
+	 	
+	 } else if (e.which == 76) {/* k */
+	 	document.getElementById("video9").style.opacity = "1";
+	 	myAudio9.volume = 1.0;
+
+	 } else if (e.which == 186) {/* semi-colon */
+	 	document.getElementById("video10").style.opacity = "1";
+	 	myAudio10.volume = 1.0;
+
+ 	}
+ 	console.log(e.keyCode);
+ } 
+
+// WHEN KEY GOT RELEASED SOUND GETS PAUSED
+
+function keyupFunction(e) {
+  // var y = e.key;
+	if(e.which == 65){/* a */
+		document.getElementById("video1").style.opacity = "0";
+		myAudio.volume = 0;
+		
+	 } else if (e.which == 83) {/* s */
+	 	document.getElementById("video2").style.opacity = "0";
+	 	myAudio2.volume = 0;
+	 	
+	 } else if (e.which == 68) {/* d */
+	 	document.getElementById("video3").style.opacity = "0";
+	 	myAudio3.volume = 0;
+
+	 } else if (e.which == 70) {/* f */
+	 	document.getElementById("video4").style.opacity = "0";
+	 	myAudio4.volume = 0;
+	 	
+	 } else if (e.which == 71) {/* g */
+	 	document.getElementById("video5").style.opacity = "0";
+	 	myAudio5.volume = 0;
+
+	 } else if (e.which == 72) {/* h */
+	 	document.getElementById("video6").style.opacity = "0";
+	 	myAudio6.volume = 0;
+	 	
+	 } else if (e.which == 74) {/* i */
+	 	document.getElementById("video7").style.opacity = "0";
+	 	myAudio7.volume = 0;
+
+	 } else if (e.which == 75) {/* j */
+	 	document.getElementById("video8").style.opacity = "0";
+	 	myAudio8.volume = 0;
+	 	
+	 } else if (e.which == 76) {/* k */
+	 	document.getElementById("video9").style.opacity = "0";
+	 	myAudio9.volume = 0;
+
+	 } else if (e.which == 186) {/* semi-colon */
+	 	document.getElementById("video10").style.opacity = "0";
+	 	myAudio10.volume = 0;
+ 	}
+ 	console.log(e);
+ }
+
+myAudio.play();
+myAudio.volume = 0;
+myAudio2.play();
+myAudio2.volume = 0;
+myAudio3.play();
+myAudio3.volume = 0;
+myAudio4.play();
+myAudio4.volume = 0;
+myAudio5.play();
+myAudio5.volume = 0;
+myAudio6.play();
+myAudio6.volume = 0;
+myAudio7.play();
+myAudio7.volume = 0;
+myAudio8.play();
+myAudio8.volume = 0;
+myAudio9.play();
+myAudio9.volume = 0;
+myAudio10.play();
+myAudio10.volume = 0;
+// audio file 2 press G
+//function keydownFunction(e) {
+//	var z = e.key;
+//	if(e.which == 71){
+//		x.play();
+//	 }
+//}
+
+//function keyupFunction(e) {
+//    var z = e.key;
+//	if(e.which == 71){
+//		x.pause();
+//	 }
+//}
+
+
